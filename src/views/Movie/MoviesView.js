87bc4629a079ce23873e49a1a43eaa765eb3047e
Anton Honcharov <antonhoncharovv@gmail.com>
2022-06-26 20:10:01 +0300

    add search value in to the input

--- src/views/Movie/MoviesView.js
+++ src/views/Movie/MoviesView.js
@@ -15,6 +15,7 @@ export default function MoviesView() {
     const handleSubmit = event => {
         event.preventDefault();
         setSearchParams({ query: searchValue }); 
+        setSearchValue(searchValue);
     }
 
     useEffect(() => {
